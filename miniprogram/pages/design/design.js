1485093f95d5c0fac21876bd8f5e01a8193e8a2b
qianqiu <2897279309@qq.com>
2025-03-24 18:28:46 +0800

    fix: 修复不能点击bug

--- miniprogram/pages/design/design.js
+++ miniprogram/pages/design/design.js
@@ -62,11 +62,11 @@ Page({
       selectedMarker: null
     })
   },
-  // gotoMarks(e) {
-  //   wx.navigateTo({
-  //     url: '/pages/outer/outer?url=' + e.currentTarget.dataset.jumplink,
-  //   })
-  // },
+  gotoLocations(e) {
+    wx.navigateTo({
+      url: '/pages/outer/outer?url=' + e.currentTarget.dataset.jumplink,
+    })
+  },
   // 标记点点击
   onMarkerTap(e) {
     console.log(e);
